17f6d6f78f1f0e86bb585af68b072cc1be54bca3
King I Likes Minecraft <96319931+KingILikesMinecraft@users.noreply.github.com>
2023-10-18 11:37:14 -0400

    Update conftest.py

--- test_bot/conftest.py
+++ test_bot/conftest.py
@@ -4,8 +4,8 @@ import os
 
 
 def pytest_sessionfinish(session, exitstatus):
-    shutil.copyfile("correct_lichess.py", "lichess.py")
-    os.remove("correct_lichess.py")
+    shutil.copyfile("correct_playstrategy.py", "playstrategy.py")
+    os.remove("correct_playstrategy.py")
     if os.path.exists("TEMP"):
         shutil.rmtree("TEMP")
     if os.path.exists("logs"):
